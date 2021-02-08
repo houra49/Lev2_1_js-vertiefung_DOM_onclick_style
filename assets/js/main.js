41fa63285deb0f4b44d126fcbccf9fa3a290204d
houra49 <houra.shamshyrcar@gmail.com>
2021-02-08 13:22:52 +0100

    Lev2_1_js-vertiefung_DOM_onclick_style

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const bg = document.getElementById("navHome")
+function changeHome() {
+    bg.classList.add("color")
+    document.getElementById("navHome").innerHTML = "hjgjhg"
+    document.getElementById("navHome").style.backgroundColor = color
+}
